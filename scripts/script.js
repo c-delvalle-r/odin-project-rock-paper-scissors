eb5d8514a4758ca19cf169209b043737db8eea13
Carlos Dv <delvallereyes.carlos@gmail.com>
2026-08-06 16:12:42 -0600

    Add comments describing the problem and the steps
    Added comments to the script.js file describing the
    problem in plain English and breaking the problem
    into smaller subproblems.

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/*
+DESCRIBE:
+I want to create a rock-paper-scissors game that runs on the terminal only. The game is played with one human aganst the computer.
+The computer picks one at random, the human picks one through a browser prompt. There is a scoreboard that keeps track of the
+number of wins each players has.
+*/
+
+// The round BEGINS
+// The computer picks one option randomly
+
+
+// The human gets prompted to input an option
+// The input gets validated
+// The winner is decided
+// The winner counter is incremented by one
+// You get the option to play another round
